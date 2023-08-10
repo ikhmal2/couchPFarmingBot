b045675f88e16de70ba5c08f20b2ff1e6a2133ad
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2023-08-10 20:02:04 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+import keyboard
+import numpy as np
+import random
+import win32api
+import win32con
+
+time.sleep(2)
+
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
